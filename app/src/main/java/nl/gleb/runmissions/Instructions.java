bc13b5cf655b4b4e0a137d365e0ac2cbb9a32eb6
Gleb <gleb.satyukov@gmail.com>
2014-10-25 15:57:58 +0200

    Added inflater to inflate haptic instructions layout on createView of the fragment.

--- app/src/main/java/nl/gleb/runmissions/Instructions.java
+++ app/src/main/java/nl/gleb/runmissions/Instructions.java
@@ -1,9 +1,20 @@
 package nl.gleb.runmissions;
 
-import android.app.Fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Created by Gleb on 25/10/14.
  */
 public class Instructions extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.instructions, container, false);
+    }
 }
