1dc79891988d519425b3b02f53e286f3947d21e4
Gleb <gleb.satyukov@gmail.com>
2014-10-26 21:33:13 +0100

    Hooked up the missions listview inside the fragment.

--- app/src/main/java/nl/gleb/runmissions/MissionList.java
+++ app/src/main/java/nl/gleb/runmissions/MissionList.java
@@ -8,6 +8,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ListView;
 
 /**
  * Created by Gleb on 26/10/14.
@@ -15,6 +16,8 @@ import android.widget.Button;
 public class MissionList extends Fragment {
 
     private static final String ARG_SECTION_NUMBER = "section_number";
+
+    ListView missions_list;
 
     public static MissionList newInstance(int sectionNumber) {
         MissionList fragment = new MissionList();
@@ -34,5 +37,13 @@ public class MissionList extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.mission_list, container, false);
+    }
+
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        missions_list = (ListView) getActivity().findViewById(R.id.missionList);
     }
 }
