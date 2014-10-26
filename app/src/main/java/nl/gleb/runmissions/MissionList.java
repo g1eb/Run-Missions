11c99b5e4243d0e9f2aed7e280817f8fbf15c786
Gleb <gleb.satyukov@gmail.com>
2014-10-26 21:48:44 +0100

    Set up a simple list adapter for the misions list and implemented a simple on item click listener for the missions.

--- app/src/main/java/nl/gleb/runmissions/MissionList.java
+++ app/src/main/java/nl/gleb/runmissions/MissionList.java
@@ -7,19 +7,20 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 /**
  * Created by Gleb on 26/10/14.
  */
-public class MissionList extends Fragment {
+public class MissionList extends Fragment implements AdapterView.OnItemClickListener {
 
     private static final String ARG_SECTION_NUMBER = "section_number";
 
     ListView missions_list;
-    String[] missions = new String[]{getString(R.string.title_mission1),
-            getString(R.string.title_mission2),
-            getString(R.string.title_mission3)};
+    String[] missions;
 
     public static MissionList newInstance(int sectionNumber) {
         MissionList fragment = new MissionList();
@@ -46,6 +47,33 @@ public class MissionList extends Fragment {
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
+        missions = new String[]{
+                getString(R.string.title_mission1),
+                getString(R.string.title_mission2),
+                getString(R.string.title_mission3)
+        };
+
         missions_list = (ListView) getActivity().findViewById(R.id.missionList);
+        missions_list.setAdapter(new ArrayAdapter<String>(
+                getActivity().getApplicationContext(),
+                android.R.layout.simple_list_item_1,
+                missions
+        ));
+        missions_list.setOnItemClickListener(this);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        switch (position) {
+            case 0:
+                Toast.makeText(getActivity().getApplicationContext(), "Mission selected: "+missions[position], Toast.LENGTH_SHORT).show();
+                break;
+            case 1:
+                Toast.makeText(getActivity().getApplicationContext(), "Mission selected: "+missions[position], Toast.LENGTH_SHORT).show();
+                break;
+            case 2:
+                Toast.makeText(getActivity().getApplicationContext(), "Mission selected: "+missions[position], Toast.LENGTH_SHORT).show();
+                break;
+        }
     }
 }
