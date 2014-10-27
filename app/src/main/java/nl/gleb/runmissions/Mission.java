7efd2e23262fec1bfc53002a3b849d322cf1785c
Gleb <gleb.satyukov@gmail.com>
2014-10-27 02:31:35 +0100

    Added some basic functionality to the mission fragment.

--- app/src/main/java/nl/gleb/runmissions/Mission.java
+++ app/src/main/java/nl/gleb/runmissions/Mission.java
@@ -1,9 +1,50 @@
 package nl.gleb.runmissions;
 
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 /**
  * Created by Gleb on 27/10/14.
  */
 public class Mission extends Fragment {
+
+    private static final String ARG_SECTION_TITLE = "section_title";
+    Comm comm;
+    static String mission_title;
+
+    public static Mission newInstance(String mission) {
+        mission_title = mission;
+        Mission fragment = new Mission();
+        Bundle args = new Bundle();
+        args.putString(ARG_SECTION_TITLE, mission);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        comm.setTitle(mission_title);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.mission, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        TextView test = (TextView) getActivity().findViewById(R.id.test);
+        test.setText(mission_title);
+    }
+
 }
