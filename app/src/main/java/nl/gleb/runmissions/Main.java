c6f23de3566dce84702b38ef44cbbd4ce3c23618
Gleb <gleb.satyukov@gmail.com>
2014-10-29 16:07:18 +0100

    Added get location services code, getting and displaying current location in a toast on connect.

--- app/src/main/java/nl/gleb/runmissions/Main.java
+++ app/src/main/java/nl/gleb/runmissions/Main.java
@@ -2,6 +2,8 @@ package nl.gleb.runmissions;
 
 import android.app.Activity;
 import android.app.Dialog;
+import android.content.Intent;
+import android.content.IntentSender;
 import android.location.Location;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -15,6 +17,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.ErrorDialogFragment;
@@ -75,7 +78,6 @@ public class Main extends ActionBarActivity
     @Override
     protected void onResume() {
         super.onResume();
-        mCurrentLocation = mLocationClient.getLastLocation();
     }
 
     @Override
@@ -164,6 +166,20 @@ public class Main extends ActionBarActivity
         actionBar.setTitle(data);
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case CONNECTION_FAILURE_RESOLUTION_REQUEST :
+                // If the result code is Activity.RESULT_OK
+                switch (resultCode) {
+                    case Activity.RESULT_OK :
+                        // Try the request again
+                        break;
+                }
+        }
+    }
+
     private boolean servicesConnected() {
         // Check that Google Play services is available
         int resultCode =
@@ -203,17 +219,44 @@ public class Main extends ActionBarActivity
 
     @Override
     public void onConnected(Bundle bundle) {
-
+        Toast.makeText(this, "Connected", Toast.LENGTH_SHORT).show();
+        mCurrentLocation = mLocationClient.getLastLocation();
+        Toast.makeText(getApplicationContext(), mCurrentLocation.toString(), Toast.LENGTH_LONG).show();
     }
 
     @Override
     public void onDisconnected() {
-
+        Toast.makeText(this, "Disconnected. Please re-connect.",
+                Toast.LENGTH_SHORT).show();
     }
 
     @Override
     public void onConnectionFailed(ConnectionResult connectionResult) {
+        if (connectionResult.hasResolution()) {
+            try {
+                connectionResult.startResolutionForResult(
+                        this,
+                        CONNECTION_FAILURE_RESOLUTION_REQUEST);
+            } catch (IntentSender.SendIntentException e) {
+                e.printStackTrace();
+            }
+        } else {
+            Dialog errorDialog = GooglePlayServicesUtil.getErrorDialog(
+                    connectionResult.getErrorCode(),
+                    this,
+                    CONNECTION_FAILURE_RESOLUTION_REQUEST);
 
+            // If Google Play services can provide an error dialog
+            if (errorDialog != null) {
+                // Create a new DialogFragment for the error dialog
+                ErrorDialog errorFragment = new ErrorDialog();
+                // Set the dialog in the DialogFragment
+                errorFragment.setDialog(errorDialog);
+                // Show the error dialog in the DialogFragment
+                errorFragment.show(getSupportFragmentManager(),
+                        "Location Updates");
+            }
+        }
     }
 
 
