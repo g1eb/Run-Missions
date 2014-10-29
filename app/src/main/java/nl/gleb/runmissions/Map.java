cf7cd2574b8be060570fc784f293a44d1da965b8
Gleb <gleb.satyukov@gmail.com>
2014-10-29 20:03:02 +0100

    Added the required functionality to the map fragment.

--- app/src/main/java/nl/gleb/runmissions/Map.java
+++ app/src/main/java/nl/gleb/runmissions/Map.java
@@ -1,7 +1,44 @@
 package nl.gleb.runmissions;
 
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
 /**
  * Created by Gleb on 29/10/14.
  */
-public class Map {
+public class Map extends Fragment {
+    private static final String ARG_SECTION_NUMBER = "section_number";
+
+    private Button startBtn, leftBtn, rightBtn, accelerateBtn, errorBtn, closerBtn;
+
+    public static Map newInstance(int sectionNumber) {
+        Map fragment = new Map();
+        Bundle args = new Bundle();
+        args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        ((Main) activity).onSectionAttached(
+                getArguments().getInt(ARG_SECTION_NUMBER));
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.map, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
 }
