9368717b1cc3571364b2ef3260d6e813af2b94d2
Gleb <gleb.satyukov@gmail.com>
2014-11-02 22:49:14 +0100

    Specified the 'next' button functionality of the soft keyboard submitting the form when everything is filled in.

--- app/src/main/java/nl/gleb/runmissions/Login.java
+++ app/src/main/java/nl/gleb/runmissions/Login.java
@@ -1,17 +1,19 @@
 package nl.gleb.runmissions;
 
 import android.app.Activity;
-import android.app.ProgressDialog;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 /**
  * Created by Gleb on 02/11/14.
@@ -45,22 +47,38 @@ public class Login extends Fragment {
         email_input = (EditText) getActivity().findViewById(R.id.email);
         password_input = (EditText) getActivity().findViewById(R.id.password);
 
+        password_input.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                boolean handled = false;
+                if (actionId == EditorInfo.IME_ACTION_DONE) {
+                    submitCredentials();
+                    handled = true;
+                }
+                return handled;
+            }
+        });
+
         login_button = (Button) getActivity().findViewById(R.id.login_button);
         login_button.setOnClickListener(new View.OnClickListener(){
 
             @Override
             public void onClick(View v) {
-                // Hide soft keyboard
-                InputMethodManager imm = (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
-                imm.hideSoftInputFromWindow(login_button.getWindowToken(), 0);
-
-                String email = email_input.getText().toString().trim();
-                String password = password_input.getText().toString().trim();
-
-                if (email != null && password != null) {
-                    comm.handleLogin(email, password);
-                }
+                submitCredentials();
             }
         });
+    }
+
+    private void submitCredentials() {
+        // Hide soft keyboard
+        InputMethodManager imm = (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(login_button.getWindowToken(), 0);
+
+        String email = email_input.getText().toString().trim();
+        String password = password_input.getText().toString().trim();
+
+        if (email != null && password != null) {
+            comm.handleLogin(email, password);
+        }
     }
 }
