9bdcd7313c6d19aafe4c61cbd956edd108364d56
Gleb <gleb.satyukov@gmail.com>
2014-11-02 21:45:37 +0100

    Renamed Main comm back to comm and converted LoginComm into a fragment.

--- app/src/main/java/nl/gleb/runmissions/Comm.java
+++ app/src/main/java/nl/gleb/runmissions/Comm.java
@@ -3,6 +3,6 @@ package nl.gleb.runmissions;
 /**
  * Created by Gleb on 26/10/14.
  */
-public interface MainComm {
+public interface Comm {
     public void setTitle(String data);
 }
--- app/src/main/java/nl/gleb/runmissions/LoginComm.java
+++
@@ -1,8 +0,0 @@
-package nl.gleb.runmissions;
-
-/**
- * Created by Gleb on 02/11/14.
- */
-public interface LoginComm {
-    public void logout();
-}
