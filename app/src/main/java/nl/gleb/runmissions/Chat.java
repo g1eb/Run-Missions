342fb55133d503edacd9fe9e5985c4413dcd0bb4
Gleb <gleb.satyukov@gmail.com>
2014-11-03 00:05:58 +0100

    Added initial profile and chat fragment classes.

---
+++ app/src/main/java/nl/gleb/runmissions/Chat.java
@@ -0,0 +1,37 @@
+package nl.gleb.runmissions;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Gleb on 03/11/14.
+ */
+public class Chat extends Fragment {
+    Comm comm;
+
+    public static Profile newInstance() {
+        return new Profile();
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        comm = (Comm) activity;
+        comm.setTitle(getString(R.string.title_chat));
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.chat, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+}
