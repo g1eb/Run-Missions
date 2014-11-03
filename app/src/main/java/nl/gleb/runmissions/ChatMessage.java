053f557a9f8538a11eb5daf70f313cc4a82e1787
Gleb <gleb.satyukov@gmail.com>
2014-11-03 13:12:05 +0100

    Added the firebase required ChatMessage class that contains an author and a message.

--- app/src/main/java/nl/gleb/runmissions/ChatMessage.java
+++ app/src/main/java/nl/gleb/runmissions/ChatMessage.java
@@ -1,7 +1,29 @@
 package nl.gleb.runmissions;
 
 /**
- * Created by Gleb on 03/11/14.
+ * User: greg
+ * Date: 6/21/13
+ * Time: 2:38 PM
  */
 public class ChatMessage {
+
+    private String message;
+    private String author;
+
+    // Required default constructor for Firebase object mapping
+    @SuppressWarnings("unused")
+    private ChatMessage() { }
+
+    ChatMessage(String message, String author) {
+        this.message = message;
+        this.author = author;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
 }
