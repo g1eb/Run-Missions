c6db600c6267b83011eb92b0e07f57a41108c242
Gleb <gleb.satyukov@gmail.com>
2014-11-03 23:10:39 +0100

    Moved the chat list adapter to the chat fragment to solve the fragment-activity communication issues.

--- app/src/main/java/nl/gleb/runmissions/Chat.java
+++ app/src/main/java/nl/gleb/runmissions/Chat.java
@@ -14,16 +14,27 @@ import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.firebase.client.ValueEventListener;
+
 /**
  * Created by Gleb on 03/11/14.
  */
 public class Chat extends Fragment {
 
+    private static final String USERNAME = "username";
+
     Comm comm;
     EditText input;
+    String username;
+    private ValueEventListener connectedListener;
+    private ChatListAdapter chatListAdapter;
 
-    public static Profile newInstance() {
-        return new Profile();
+    public static Chat newInstance(String username) {
+        Chat fragment = new Chat();
+        Bundle args = new Bundle();
+        args.putString(USERNAME, username);
+        fragment.setArguments(args);
+        return fragment;
     }
 
     @Override
@@ -41,8 +52,8 @@ public class Chat extends Fragment {
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-
         // Setup our input methods. Enter key on the keyboard or pushing the send button
+
         input = (EditText) getActivity().findViewById(R.id.messageInput);
         input.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
@@ -60,11 +71,25 @@ public class Chat extends Fragment {
                 sendMessage();
             }
         });
+
+        // Setup our view and list adapter. Ensure it scrolls to the bottom as data changes
+        final ListView listView = (ListView) getActivity().findViewById(R.id.chat_list);
+
+        // Tell our list adapter that we only want 50 messages at a time
+        chatListAdapter = new ChatListAdapter(Main.chatRef.limit(50), getActivity(), R.layout.chat_message, getArguments().getString(USERNAME));
+        listView.setAdapter(chatListAdapter);
+        chatListAdapter.registerDataSetObserver(new DataSetObserver() {
+            @Override
+            public void onChanged() {
+                super.onChanged();
+                listView.setSelection(chatListAdapter.getCount() - 1);
+            }
+        });
     }
 
     private void sendMessage() {
         String message = input.getText().toString().trim();
-        if ( !message.equals("") ) {
+        if (!message.equals("")) {
             comm.sendMessage(message);
             input.setText("");
         }
