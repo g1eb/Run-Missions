ff84deaeba115fc629bec2c5662dc92820399c87
Gleb <gleb.satyukov@gmail.com>
2014-11-04 00:52:23 +0100

    Added button/text decalarations inside signup fragment class.

--- app/src/main/java/nl/gleb/runmissions/Signup.java
+++ app/src/main/java/nl/gleb/runmissions/Signup.java
@@ -4,10 +4,16 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * Created by Gleb on 04/11/14.
@@ -15,6 +21,8 @@ import android.widget.SeekBar;
 public class Signup extends Fragment {
 
     Comm comm;
+    private EditText signup_email, signup_username, signup_password, signup_confirm_password;
+    private Button signup_button;
 
     public static Signup newInstance() {
         return new Signup();
@@ -35,5 +43,42 @@ public class Signup extends Fragment {
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+
+        signup_email = (EditText) getActivity().findViewById(R.id.signup_email);
+        signup_username = (EditText) getActivity().findViewById(R.id.signup_username);
+        signup_password = (EditText) getActivity().findViewById(R.id.signup_password);
+        signup_confirm_password = (EditText) getActivity().findViewById(R.id.signup_confirm_password);
+
+        signup_confirm_password.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                boolean handled = false;
+                if (actionId == EditorInfo.IME_ACTION_DONE) {
+                    confirmSignup();
+                    handled = true;
+                }
+                return handled;
+            }
+        });
+
+        signup_button = (Button) getActivity().findViewById(R.id.signup_button);
+        signup_button.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                confirmSignup();
+            }
+        });
+    }
+
+    private void confirmSignup() {
+        String email = signup_email.getText().toString().trim();
+        String username = signup_username.getText().toString().trim();
+        String password1 = signup_password.getText().toString().trim();
+        String password2 = signup_confirm_password.getText().toString().trim();
+
+        if ( !password1.equals(password2) ) {
+            Toast.makeText(getActivity().getApplicationContext(), "Passwords do not match..", Toast.LENGTH_LONG).show();
+        }
     }
 }
