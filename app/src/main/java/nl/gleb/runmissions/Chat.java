951737e45762907f2faad3361358a39a9a35245c
Gleb <gleb.satyukov@gmail.com>
2014-11-03 13:09:21 +0100

    Added basic chat functions to the chat fragment, edittext with a send button.

--- app/src/main/java/nl/gleb/runmissions/Chat.java
+++ app/src/main/java/nl/gleb/runmissions/Chat.java
@@ -1,18 +1,26 @@
 package nl.gleb.runmissions;
 
 import android.app.Activity;
+import android.database.DataSetObserver;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
 
 /**
  * Created by Gleb on 03/11/14.
  */
 public class Chat extends Fragment {
+
     Comm comm;
+    EditText input;
 
     public static Profile newInstance() {
         return new Profile();
@@ -33,5 +41,32 @@ public class Chat extends Fragment {
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+
+        // Setup our input methods. Enter key on the keyboard or pushing the send button
+        input = (EditText) getActivity().findViewById(R.id.messageInput);
+        input.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int actionId, KeyEvent keyEvent) {
+                if (actionId == EditorInfo.IME_NULL && keyEvent.getAction() == KeyEvent.ACTION_DOWN) {
+                    sendMessage();
+                }
+                return true;
+            }
+        });
+
+        getActivity().findViewById(R.id.sendButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendMessage();
+            }
+        });
+    }
+
+    private void sendMessage() {
+        String message = input.getText().toString().trim();
+        if ( !message.equals("") ) {
+            comm.sendMessage(message);
+            input.setText("");
+        }
     }
 }
