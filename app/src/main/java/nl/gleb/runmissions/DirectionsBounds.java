c9a938035cdcf140e042634a68ca77a29e5ae7f5
Gleb <gleb.satyukov@gmail.com>
2014-11-08 22:02:55 +0100

    Added DirectionsBounds class to map route bounds retrieved from directions api.

---
+++ app/src/main/java/nl/gleb/runmissions/DirectionsBounds.java
@@ -0,0 +1,11 @@
+package nl.gleb.runmissions;
+
+import com.google.api.client.util.Key;
+
+public class DirectionsBounds {
+    @Key("northeast")
+    public PlaceLocation northeast;
+
+    @Key("southwest")
+    public PlaceLocation southwest;
+}
