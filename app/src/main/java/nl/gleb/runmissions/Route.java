79801f32deaedf105b3a85520779f91616179613
Gleb <gleb.satyukov@gmail.com>
2014-11-08 22:06:36 +0100

    Added directionsBounds to the route mapper.

--- app/src/main/java/nl/gleb/runmissions/Route.java
+++ app/src/main/java/nl/gleb/runmissions/Route.java
@@ -5,4 +5,7 @@ import com.google.api.client.util.Key;
 public class Route {
     @Key("overview_polyline")
     public OverviewPolyLine overviewPolyLine;
+
+    @Key("bounds")
+    public DirectionsBounds bounds;
 }
