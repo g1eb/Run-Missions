f5082249ab108a09ba33dce7db6c27b25c5c7df9
Gleb <gleb.satyukov@gmail.com>
2014-11-08 15:46:37 +0100

    Added place icon as defined by google, not sure what to do with this now, will see later.

--- app/src/main/java/nl/gleb/runmissions/Place.java
+++ app/src/main/java/nl/gleb/runmissions/Place.java
@@ -3,14 +3,18 @@ package nl.gleb.runmissions;
 import com.google.api.client.util.Key;
 
 public class Place {
-    @Key("name")
-    public String name;
 
     @Key("id")
     public String id;
 
+    @Key("name")
+    public String name;
+
     @Key("types")
     public String[] types;
+
+    @Key("icon")
+    public String icon;
 
     @Key("vicinity")
     public String vicinity;
