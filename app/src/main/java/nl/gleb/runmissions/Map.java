9decc91ac7a4bcaab8f76b892940931cd5500962
Gleb <gleb.satyukov@gmail.com>
2014-11-08 15:25:04 +0100

    Added a call to the google places api to get the places of interest.

--- app/src/main/java/nl/gleb/runmissions/Map.java
+++ app/src/main/java/nl/gleb/runmissions/Map.java
@@ -4,12 +4,25 @@ import android.app.Activity;
 import android.content.res.Resources;
 import android.location.Location;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.http.GenericUrl;
+import com.google.api.client.http.HttpRequest;
+import com.google.api.client.http.HttpRequestFactory;
+import com.google.api.client.http.HttpRequestInitializer;
+import com.google.api.client.http.HttpResponse;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.json.JsonObjectParser;
+import com.google.api.client.json.jackson.JacksonFactory;
+
+import java.io.IOException;
+import java.util.List;
 
 /**
  * Created by Gleb on 29/10/14.
@@ -18,6 +31,10 @@ public class Map extends SupportMapFragment implements GoogleMap.OnMapLoadedCall
 
     private static GoogleMap map;
     Comm comm;
+    static final HttpTransport HTTP_TRANSPORT = AndroidHttp.newCompatibleTransport();
+    static final com.google.api.client.json.JsonFactory JSON_FACTORY = new JacksonFactory();
+
+    HttpRequestFactory requestFactory;
 
     public static Map newInstance() {
         return new Map();
@@ -33,6 +50,16 @@ public class Map extends SupportMapFragment implements GoogleMap.OnMapLoadedCall
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        requestFactory = HTTP_TRANSPORT.createRequestFactory(
+                new HttpRequestInitializer() {
+                    @Override
+                    public void initialize(HttpRequest request) {
+                        request.setParser(new JsonObjectParser(JSON_FACTORY));
+                    }
+                }
+        );
+
         initMap();
     }
 
@@ -81,10 +108,40 @@ public class Map extends SupportMapFragment implements GoogleMap.OnMapLoadedCall
                 .build();
         map.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition),
                 res.getInteger(R.integer.map_animation_duration), null);
+
+        getPlaces();
     }
 
     @Override
     public void onMapLoaded() {
         setupMap();
+    }
+
+    /*
+     * Get places of interest around the user
+     */
+    public void getPlaces() {
+
+        Location l = ((Main) getActivity()).mCurrentLocation;
+
+        GenericUrl url = new GenericUrl("https://maps.googleapis.com/maps/api/place/nearbysearch/json");
+        url.put("key", "AIzaSyCbFFLGTKvJh_on6sRgwp0mcz0Rl-B_ijk");
+        url.put("location", l.getLatitude() + "," + l.getLongitude());
+        url.put("radius", 5000);
+
+        HttpRequest request;
+        try {
+            request = requestFactory.buildGetRequest(url);
+            HttpResponse httpResponse = request.execute();
+
+            PlacesList result = httpResponse.parseAs(PlacesList.class);
+
+            List<Place> places = result.results;
+            for (Place place : places) {
+                Log.d("MAP", place.name);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
