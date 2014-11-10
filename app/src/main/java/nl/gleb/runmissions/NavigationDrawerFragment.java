59d0b21a34c8b880f63461a811eb4ef12bedf6af
Gleb <gleb.satyukov@gmail.com>
2014-11-10 02:05:15 +0100

    Use our custom list adapter to build the nav drawer.

--- app/src/main/java/nl/gleb/runmissions/NavigationDrawerFragment.java
+++ app/src/main/java/nl/gleb/runmissions/NavigationDrawerFragment.java
@@ -1,16 +1,16 @@
 package nl.gleb.runmissions;
 
-import android.support.v7.app.ActionBarActivity;
 import android.app.Activity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.ActionBarDrawerToggle;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
 import android.content.SharedPreferences;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -18,9 +18,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.Toast;
 
 /**
  * Fragment used for managing interactions for and presentation of a navigation drawer.
@@ -97,18 +95,13 @@ public class NavigationDrawerFragment extends Fragment {
                 selectItem(position);
             }
         });
-        mDrawerListView.setAdapter(new ArrayAdapter<String>(
-                getActionBar().getThemedContext(),
-                android.R.layout.simple_list_item_1,
-                android.R.id.text1,
-                new String[]{
-                        getString(R.string.title_profile),
-                        getString(R.string.title_map),
-                        getString(R.string.title_chat),
-                        getString(R.string.title_missions),
-                        getString(R.string.title_instructions)
-                }));
+
+        mDrawerListView.setAdapter(new NavDrawerItemListAdapter(
+                getActivity().getApplicationContext(),
+                new Integer[]{R.drawable.ic_action_profile}
+        ));
         mDrawerListView.setItemChecked(mCurrentSelectedPosition, true);
+
         return mDrawerListView;
     }
 
