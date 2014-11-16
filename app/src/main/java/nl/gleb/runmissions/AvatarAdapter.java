939f79b92c382109ab3e1353e887ab5327518f67
Gleb <gleb.satyukov@gmail.com>
2014-11-16 22:45:27 +0100

    Added AvatarAdapter class that will be serving avatar images to the ViewPager inside the profile view.

---
+++ app/src/main/java/nl/gleb/runmissions/AvatarAdapter.java
@@ -0,0 +1,52 @@
+package nl.gleb.runmissions;
+
+import android.content.Context;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+/**
+ * Created by Gleb on 16/11/14.
+ */
+public class AvatarAdapter extends PagerAdapter {
+
+    Context context;
+
+    private int[] AvatarImages = new int[]{
+            R.drawable.a_1,
+            R.drawable.a_2,
+            R.drawable.a_3
+    };
+
+    AvatarAdapter(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return AvatarImages.length;
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return view == ((ImageView) object);
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+        ImageView imageView = new ImageView(context);
+        int padding = context.getResources().getDimensionPixelSize(R.dimen.activity_horizontal_margin);
+        imageView.setPadding(padding, padding, padding, padding);
+        imageView.setScaleType(ImageView.ScaleType.FIT_CENTER);
+        imageView.setImageResource(AvatarImages[position]);
+        ((ViewPager) container).addView(imageView, 0);
+        return imageView;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        ((ViewPager) container).removeView((ImageView) object);
+    }
+}
