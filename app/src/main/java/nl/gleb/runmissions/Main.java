448990df232e6e7bb5e670a46ab12c487a60a09f
Gleb <gleb.satyukov@gmail.com>
2014-12-14 18:22:04 +0100

    Revert "Removed the sprint and getting closer haptic signals from the handle hapticFeedback function."
    This reverts commit 8c39b6f6e113fbb446874c53f8dad45582a7d7f3.

--- app/src/main/java/nl/gleb/runmissions/Main.java
+++ app/src/main/java/nl/gleb/runmissions/Main.java
@@ -486,6 +486,10 @@ public class Main extends ActionBarActivity
                 vibrator.vibrate(getPattern("left"), -1);
             } else if (step.html_instructions.toLowerCase().contains("right")) {
                 vibrator.vibrate(getPattern("right"), -1);
+            } else if (step.html_instructions.toLowerCase().contains("head")) {
+                vibrator.vibrate(getPattern("accelerate"), -1);
+            } else if (step.html_instructions.toLowerCase().contains("continue")) {
+                vibrator.vibrate(getPattern("closer"), -1);
             }
 
             feedbackCounter = feedbackRate;
