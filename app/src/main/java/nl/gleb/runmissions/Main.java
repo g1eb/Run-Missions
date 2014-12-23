0f3f30795d32b97ba52d260c9c2a3626135a4ed0
Gleb <gleb.satyukov@gmail.com>
2014-12-23 16:19:09 +0100

    Faster update rates for the location manager.

--- app/src/main/java/nl/gleb/runmissions/Main.java
+++ app/src/main/java/nl/gleb/runmissions/Main.java
@@ -53,8 +53,8 @@ public class Main extends ActionBarActivity
      * Constants
      */
     private final static int CONNECTION_FAILURE_RESOLUTION_REQUEST = 9000;
-    private static final long UPDATE_INTERVAL = 10000;
-    private static final long FASTEST_INTERVAL = 5000;
+    private static final long UPDATE_INTERVAL = 3000;
+    private static final long FASTEST_INTERVAL = 3000;
     private static final int FEEDBACK_RANGE = 25;
     private static final int FINISH_RANGE = 25;
     static final String SETTINGS_TAG = "SETTINGS";
